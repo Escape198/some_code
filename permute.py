1ff116765e36a7c8586cbc6e7e027529d65160c8
Escape198 <101140452+Escape198@users.noreply.github.com>
2022-04-19 15:36:38 +0300

    Update permute.py

--- permute.py
+++ permute.py
@@ -2,27 +2,26 @@ import itertools
 import sys
 import time
 
+def run(n):
+    n = int(sys.argv[1])
+    l = [0]*(n-1)
+    start_time = time.time()
+    for i in range(n+1):
+        l.append(i)
+    permute = list(itertools.permutations(l))
 
-n = int(sys.argv[1]) 
-l = [0]*(n-1)
+    list_permute = []
+    for i in permute:
+        string = ''
+        for k in i:
+            string += str(k)
+        list_permute.append(string + '\n')
+    print("%s секунд" % (time.time() - start_time))
 
-start_time = time.time()
-for i in range(n+1):
-    l.append(i)
-permute = list(itertools.permutations(l))
+    f = open('text.txt', 'w')
+    for index in list_permute:
+        f.write(index)
 
-list_permute = []
-for i in permute:
-    string = ''
-    for k in i:
-        string += str(k)
-    list_permute.append(string + '\n')
-print("%s секунд" % (time.time() - start_time))
+    f.close()
 
-f = open('text.txt', 'w')
-for index in list_permute:
-    f.write(index)
-
-f.close()
-
-print(f'Для чисел от 0 до {n} существует {len(list_permute)} перестановки(ок)')
+    print(f'Для чисел от 0 до {n} существует {len(list_permute)} перестановки(ок)')
