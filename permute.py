e9741852ae3dff44048b02b1ac7d87ed8a2e3f12
Escape198 <101140452+Escape198@users.noreply.github.com>
2022-04-19 15:02:45 +0300

    Add files via upload

---
+++ permute.py
@@ -0,0 +1,28 @@
+import itertools
+import sys
+import time
+
+
+n = int(sys.argv[1]) 
+l = [0]*(n-1)
+
+start_time = time.time()
+for i in range(n+1):
+    l.append(i)
+permute = list(itertools.permutations(l))
+
+list_permute = []
+for i in permute:
+    string = ''
+    for k in i:
+        string += str(k)
+    list_permute.append(string + '\n')
+print("%s секунд" % (time.time() - start_time))
+
+f = open('text.txt', 'w')
+for index in list_permute:
+    f.write(index)
+
+f.close()
+
+print(f'Для чисел от 0 до {n} существует {len(list_permute)} перестановки(ок)')
